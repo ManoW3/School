a1b1159be6d9a702b04f7ade56a7275c7300fa44
ManoW3 <manoisnotcool@gmail.com>
2024-12-03 09:06:45 -0500

    Added couts of the questions instead of just comments

--- words.cpp
+++ words.cpp
@@ -13,21 +13,30 @@ double logBase(double a, double b)
 
 int main()
 {
+    
     // I have a right triangle shaped yard and need to put a fence around it. If it is 12 feet long and 5 feet wide, how much total fence will I need?
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "I have a right triangle shaped yard and need to put a fence around it. If it is 12 feet long and 5 feet wide, how much total fence will I need?" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
     int a = 12;
     int b = 5;
     int csq = pow(a, 2) + pow(b, 2);        // Finds the hypotenuse using the pythagorean theorem
     int hypot = sqrt(csq);
     int totalFence = a + b + hypot;         // Add its all together for the perimiter
-    cout << "You will need " << totalFence << " Feet of fence" << endl;
+    cout << "You will need " << totalFence << " Feet of fence" << endl << endl;
 
 
     // John is walking ALONG a unit circle, he walks 30º (Up from 1, 0), what is his current x position?
-
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "John is walking ALONG a unit circle, he walks 30º (Up from 1, 0), what is his current x position?" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
     double rads = 30*(3.1415926535/180);            // Converts to Radians
-    cout << "His y position will be " << cos(rads) << " Units" << endl;
+    cout << "His y position will be " << cos(rads) << " Units" << endl << endl;
 
     // A crypto is rising at 6% per day, if the coin starts at $0.01, how long will it take until it overtakes Dow Jones ($44,782.00 as of writing this problem)
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "A crypto is rising at 6% per day, if the coin starts at $0.01, how long will it take until it overtakes Dow Jones ($44,782.00 as of writing this problem)" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
     double curPrice = 0.01;                                // Current Price
     double increase = 0.07;                                // increase per day
     double targetValue = 44782;                            // The target value (price of the stock)
